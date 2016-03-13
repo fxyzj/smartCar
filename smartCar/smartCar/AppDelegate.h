77b6389994db752762090d702c47b1640c7170a7
fxyzj <778607758@qq.com>
2016-03-13 11:30:29 +0800

    初始化项目

---
+++ smartCar/smartCar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  smartCar
+//
+//  Created by 符晓勇 on 16/3/13.
+//  Copyright © 2016年 符晓勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ smartCar/smartCar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  smartCar
+//
+//  Created by 符晓勇 on 16/3/13.
+//  Copyright © 2016年 符晓勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
